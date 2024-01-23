f335adf9d225125dcfc351bfcaffc4f419c05ef0
phiniqq724 <fahrellnew@gmail.com>
2024-01-23 10:38:18 +0700

    Pikek

---
+++ Abstract1.java
@@ -0,0 +1,6 @@
+public abstract class Abstract1 {
+    public void function() {
+        System.out.println("Apalah");
+    }
+    public abstract void pikek();
+} 
---
+++ Abstract2.java
@@ -0,0 +1,6 @@
+public class Abstract2 extends Abstract1{
+    @Override
+    public void pikek() {
+        
+    }
+}
